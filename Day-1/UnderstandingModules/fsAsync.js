ae45234b6f290f92fd7f0516f002828ba533883d
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-12 00:22:22 +0500

    Create fsAsync.js
    Practicing Async Nature of FS Modules

---
+++ Day-1/UnderstandingModules/fsAsync.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileName = 'AsyncFS.txt';
+const filePath = path.join(__dirname, fileName);
+
+// const writeFile = fs.writeFile(
+//   filePath,
+//   'Oyee Ballay Bhallay File chal gyi.',
+//   'utf-8',
+//   (err) => {
+//     if (err) console.error(err);
+//     else console.log('File Saved Success');
+//   },
+// );
+
+const readFile = fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) console.error(err);
+  else console.log('File Read Success:', data);
+});
+
+const appendFile = fs.appendFile(
+  filePath,
+  '\n File Append kr di gyi hai.',
+  'utf-8',
+  (err) => {
+    if (err) console.error(err);
+    else console.log('File Appended Successfully.');
+  },
+);
+
+// const newUpdatedFile = 'Hehehe.txt';
+// const newUpdatedFilePath = path.join(__dirname, newUpdatedFile);
+
+// const renameFile = fs.renameSync(filePath, newUpdatedFilePath);
+
+// console.log('File Renamed Successfully:', renameFile);   
+
+// const deleteFile = fs.unlinkSync(filePath);
+// console.log('Deleting file: ', deleteFile);
+
+const deleteFile = fs.unlinkSync(filePath);
+
+console.log('File deleted successfully:', deleteFile);
