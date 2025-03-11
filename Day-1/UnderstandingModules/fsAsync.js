11de83660d877b17d5c559d6633e13e4a68601d8
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-12 00:35:49 +0500

    Update fsAsync.js

--- Day-1/UnderstandingModules/fsAsync.js
+++ Day-1/UnderstandingModules/fsAsync.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const fileName = 'AsyncFS.txt';
+const fileName = 'loll.txt';
 const filePath = path.join(__dirname, fileName);
 
-// const writeFile = fs.writeFile(
-//   filePath,
-//   'Oyee Ballay Bhallay File chal gyi.',
-//   'utf-8',
-//   (err) => {
-//     if (err) console.error(err);
-//     else console.log('File Saved Success');
-//   },
-// );
+// Read File
+
+fs.writeFile(
+  filePath,
+  'Hello Apka Data thora sa casual hai.',
+  'utf-8',
+  (err) => {
+    if (err) throw err;
+    console.log('File has been created.');
+  },
+);
+
+fs.appendFile(filePath, '\nHello Updated File hai.', 'utf-8', (err) => {
+  if (err) console.error(err);
+  console.log('Data appended to file.');
+});
+
+const writeFile = fs.writeFile(
+  filePath,
+  'Oyee Ballay Bhallay File chal gyi.',
+  'utf-8',
+  (err) => {
+    if (err) console.error(err);
+    else console.log('File Saved Success');
+  },
+);
 
 const readFile = fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) console.error(err);
@@ -29,16 +46,14 @@ const appendFile = fs.appendFile(
   },
 );
 
-// const newUpdatedFile = 'Hehehe.txt';
-// const newUpdatedFilePath = path.join(__dirname, newUpdatedFile);
+const newUpdatedFile = 'Hehehe.txt';
+const newUpdatedFilePath = path.join(__dirname, newUpdatedFile);
 
-// const renameFile = fs.renameSync(filePath, newUpdatedFilePath);
+const renameFile = fs.renameSync(filePath, newUpdatedFilePath);
 
-// console.log('File Renamed Successfully:', renameFile);
+console.log('File Renamed Successfully:', renameFile);
 
-// const deleteFile = fs.unlinkSync(filePath);
-// console.log('Deleting file: ', deleteFile);
-
-const deleteFile = fs.unlink(filePath);
+const deleteFile = fs.unlinkSync(filePath);
+console.log('Deleting file: ', deleteFile);
 
 console.log('File deleted successfully:', deleteFile);
