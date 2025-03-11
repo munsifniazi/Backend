dbe5aa1475a58c2d6bdbddd1524356c2529a8d95
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-11 16:12:04 +0500

    Update path_modules.js

--- Day-1/UnderstandingModules/path_modules.js
+++ Day-1/UnderstandingModules/path_modules.js
@@ -8,3 +8,11 @@ console.log(__filename);
 const filePath = path.join('folder', 'students', 'data.txt');
 
 console.log(filePath);
+
+const parseData = path.parse(filePath);
+const resolveData = path.resolve(filePath);
+const extName = path.extname(filePath);
+const dirName = path.dirname(filePath);
+const baseName = path.basename(filePath);
+
+console.log({ parseData, extName, dirName, baseName, resolveData });
