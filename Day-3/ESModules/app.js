b28eb4bf18d1914431bbede10c9b5069c2b52713
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-14 01:15:20 +0500

    Create app.js

---
+++ Day-3/ESModules/app.js
@@ -0,0 +1,10 @@
+import { add, subtract, multiply, divide, PI } from './math.js';
+
+console.log(add(51, 27));
+console.log(subtract(51, 27));
+console.log(multiply(51, 27));
+console.log(divide(54, 27));
+
+console.log(PI);
+
+// Importing and using a function from another JavaScript file (math.js) in a JavaScript file (index.js) using ES6 modules
