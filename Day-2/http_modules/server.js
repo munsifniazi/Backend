13b2047ea523ab4e7cb272f80f245fb700ba2b6b
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-14 00:42:23 +0500

    Update server.js

--- Day-2/http_modules/server.js
+++ Day-2/http_modules/server.js
@@ -1,10 +1,22 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello Munsif Khan</h1>');
+    res.end();
+  } else if (req.url === '/about') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h1>About Munsif Khan</h1>
+      <p>Heyy I am a full stack web developer</p>`);
+    res.end();
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>404 Page Not Found</h1>');
+    res.end();
+  }
 });
-const Port = 3000;
 
-
-server.listen(Port, () => {
-  console.log(`server is running on http://localhost:${Port}`);
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`);
 });
