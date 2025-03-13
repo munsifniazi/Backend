b5f81196e068eed0a9e8ad80b243fe89def13448
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-14 01:15:17 +0500

    Create math.js

---
+++ Day-3/ESModules/math.js
@@ -0,0 +1,21 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const PI = 3.14;
+
+// Exporting multiple functions as an object
+
+export { add, subtract, multiply, divide, PI };
